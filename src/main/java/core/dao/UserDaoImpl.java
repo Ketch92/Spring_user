c6b50481e71ecfc24c6e27c02512219aae109323
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-08 17:48:53 +0200

    Implemented UserServiceImpl

---
+++ src/main/java/core/dao/UserDaoImpl.java
@@ -0,0 +1,16 @@
+package core.dao;
+
+import core.model.User;
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void add(User user) {
+    
+    }
+    
+    @Override
+    public List<User> listUsers() {
+        return null;
+    }
+}
