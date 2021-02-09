45ddb62e4a4dfdb5c06e8de84649d88f6dc927e3
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-09 09:57:54 +0200

    Fix

--- src/main/java/core/dao/UserDaoImpl.java
+++ src/main/java/core/dao/UserDaoImpl.java
@@ -5,13 +5,15 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDaoImpl implements UserDao {
-    @Autowired
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
+    
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
     
     @Override
     public void add(User user) {
