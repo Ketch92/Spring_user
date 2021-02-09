fbbd8210b665f69ed6a2c537409838fab4ab18b5
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-09 13:41:06 +0200

    Added and configured properties

--- src/main/java/core/config/AppConfig.java
+++ src/main/java/core/config/AppConfig.java
@@ -7,21 +7,30 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("db.properties")
 @ComponentScan(basePackages = {
         "core.dao",
         "core.service"
 })
 public class AppConfig {
+    private final Environment environment;
+    
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+    
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/cinema");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("Qwest900");
+        dataSource.setDriverClassName(environment.getProperty("db.driver"));
+        dataSource.setUrl(environment.getProperty("db.url"));
+        dataSource.setUsername(environment.getProperty("db.user"));
+        dataSource.setPassword(environment.getProperty("db.password"));
         return dataSource;
     }
     
@@ -31,8 +40,9 @@ public class AppConfig {
         factoryBean.setDataSource(getDataSource());
     
         Properties prop = new Properties();
-        prop.setProperty("hibernate.show_sql", "true");
-        prop.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        prop.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        prop.setProperty("hibernate.hbm2ddl.auto",
+                environment.getProperty("hibernate.hbm2ddl.auto"));
         
         factoryBean.setHibernateProperties(prop);
         factoryBean.setAnnotatedClasses(User.class);
