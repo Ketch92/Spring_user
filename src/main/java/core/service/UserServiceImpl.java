dd853eb1c99de1e10ac756a4132504804fdf3edb
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-09 10:01:27 +0200

    Fix

--- src/main/java/core/service/UserServiceImpl.java
+++ src/main/java/core/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package core.service;
 import core.dao.UserDao;
 import core.model.User;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
